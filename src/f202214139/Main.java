355f60e4882567907b58c1f916eb3e03f7b929ea
seokhawn01 <skhu202214139@gmail.com>
2025-10-19 16:51:04 +0900

    five

--- src/f202214139/Main.java
+++ src/f202214139/Main.java
@@ -5,5 +5,6 @@ public class Main {
 	public static void main(String[] args) { 
 		System.out.println("김석환"); 
 		World.run();
+		World.run2();
 		} 
 	}
--- src/f202214139/World.java
+++ src/f202214139/World.java
@@ -2,5 +2,10 @@ public class World {
     
     public static void run() { 
         System.out.println("202214139");
-         } 
+         }
+          
+         public static void run2() { 
+            System.out.println("hello world"); 
+         }
+
          }
